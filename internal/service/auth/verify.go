91c2c144809e13a1d62044242599ce7b420a4343
shinoda4 <lindesong666@gmail.com>
2025-11-18 23:39:28 +0800

    remove reductant send email functions.

--- internal/service/auth/verify.go
+++ internal/service/auth/verify.go
@@ -2,7 +2,9 @@ package auth
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"os"
 
 	"github.com/shinoda4/sd-svc-auth/pkg/email"
 	"github.com/shinoda4/sd-svc-auth/pkg/token"
@@ -27,7 +29,14 @@ func (s *Service) VerifyEmail(ctx context.Context, token string, sendEmail bool)
 	}
 
 	if sendEmail {
-		welcomeErr := email.SendWelcomeEmail(user.GetEmail(), user.GetUsername())
+		subject := fmt.Sprintf("Welcome! %s", user.GetUsername())
+		body := "Dear <b>" + user.GetUsername() + "</b>, you are already verified! Welcome to our system!"
+		emailAddress := os.Getenv("EMAIL_ADDRESS")
+		if emailAddress == "" {
+			return errors.New("EMAIL_ADDRESS environment variable not set")
+		}
+
+		welcomeErr := email.SendEmail(emailAddress, user.GetEmail(), subject, body)
 		if welcomeErr != nil {
 			return welcomeErr
 		}
